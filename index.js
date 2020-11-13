d742d0d3e6261f10daace96761870c8dbd83d998
asif-kamal <asif.kamal1489@gmail.com>
2020-11-12 21:41:51 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+//   //...
+// ]
+
+function superbowlWin(record) {
+  let win = record.find( function(obj) {return obj.result === "W"}) 
+  if(win){
+    return win.year
+  }
+  }
